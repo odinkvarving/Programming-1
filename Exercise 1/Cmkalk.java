8fed63efdad814c2cdb4f82a63e567706eac7a15
odinkvarving <55768648+odinkvarving@users.noreply.github.com>
2021-10-26 08:35:35 +0200

    Add files via upload

---
+++ Exercise 1/Cmkalk.java
@@ -0,0 +1,12 @@
+/* Ditta programit gjør om tomta te cantimeters
+*/
+
+class Cmkalk {
+	public static void main(String[] arg){
+		double centimeter = 7;
+		double tommer = centimeter * 2.54;
+
+		System.out.println( + centimeter + " centimeter tilsvarer " + tommer + " tommer " );
+	}
+}
+
